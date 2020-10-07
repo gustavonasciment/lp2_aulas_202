630c4c50942f57028f2d0378e8c11045c2da2072
Gustavo Nascimento <gustavonascimento@MacBook-Pro-de-Gustavo.local>
2020-10-07 20:42:38 -0300

    coniguração dos assets do MDB

---
+++ application/views/common/rodape.php
@@ -0,0 +1,9 @@
+
+        <script type="text/javascript" src="<?base_url() ?>assets/mdb/js/jquery.min.js"></script> 
+        <script type="text/javascript" src="<?base_url() ?>assets/mdb/js/popper.min.js"></script> 
+        <script type="text/javascript" src="<?base_url() ?>assets/mdb/js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="<?base_url() ?>assets/mdb/js/mdb.min.js"></script>
+        <script type="text/javascript"></script>
+
+    </body>
+</html>
