02486c13607f3d5228ceac1441865830b69bbf2b
Gustavo Nascimento <gustavonascimento@MacBook-Pro-de-Gustavo.local>
2020-10-08 17:31:07 -0300

    aula-4 completa

---
+++ application/libraries/Login.php
@@ -0,0 +1,12 @@
+<?php
+include_once APPPATH. 'libraries/util/CI_Object.php';
+
+class login extends CI_Object{
+
+public function verifica($email, $senha){
+    $res = $this->db->get_where('login', ['email' => $email, 'senha' => $senha]);
+    $v = $res->result_array();
+    return($v);
+}
+
+}
