14d64742329e486edaa0a525c155cf1d77c9d082
Gustavo Nascimento <gustavonascimento@MacBook-Pro-de-Gustavo.local>
2020-10-07 21:30:00 -0300

    aula-2 completa

---
+++ application/views/access/login_form.php
@@ -0,0 +1,19 @@
+<div style="height: 100vh">
+    <div class="flex-center flex-column">
+        <h3 class="mb-5">Controle Financeiro Pessoal</h3>
+
+        <form class="text-center border border-light p-5" method="POST">
+
+            <p class="h4 mb-4">Entrar</p>
+            <input type="email" id="email" name="e-mail" class="form-control mb-4" placeholder="E-mail">
+            <input type="password" id="senha" name="senha" class="form-control mb-4" placeholder="Senha">
+
+            <button class="btn btn-info btn-block my-4" type="submit">Enviar</button>
+
+            <p>não é membro?
+                <a href="">Registre-se</a>
+            </p>
+
+        </form>
+    </div>
+</div>
