bcf684a9f915cfc78fc34c866c8bd53b2c7ad950
Gustavo Nascimento <gustavonascimento@MacBook-Pro-de-Gustavo.local>
2020-10-08 21:54:55 -0300

    identação da navbar

--- application/views/common/navbar.php
+++ application/views/common/navbar.php
@@ -1,19 +1,19 @@
-<!--Navbar-->
+
 <nav class="navbar navbar-expand-lg navbar-dark primary-color">
 
-  <!-- Navbar brand -->
+ 
   <a class="navbar-brand" href="#">Controle Financeiro</a>
 
-  <!-- Collapse button -->
+
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#basicExampleNav"
     aria-controls="basicExampleNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
 
-  <!-- Collapsible content -->
+  
   <div class="collapse navbar-collapse" id="basicExampleNav">
 
-    <!-- Links -->
+ 
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
         <a class="nav-link" href="<?= base_url('home') ?>">Home
@@ -21,7 +21,7 @@
         </a>
       </li>
       
-      <!-- Dropdown -->
+   
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown"
           aria-haspopup="true" aria-expanded="false">Cadastro</a>
@@ -53,10 +53,9 @@
       </li>
 
     </ul>
-    <!-- Links -->
+
 
   </div>
-  <!-- Collapsible content -->
+
 
 </nav>
-<!--/.Navbar-->
